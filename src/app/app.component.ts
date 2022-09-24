f44c8f9dc12201953ab44a7b0115a0e5d12d8aac
Luiz Miguel <luizjarduli@gmail.com>
2022-09-24 15:11:11 -0300

    fix: reset app boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    template: `1<router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  title = 'pokedex';
-}
+export class AppComponent { }
