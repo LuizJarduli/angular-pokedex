764874d49a52d0f13a536d91e5feebf2342a0bf8
Luiz Miguel <luizjarduli@gmail.com>
2022-09-25 16:58:29 -0300

    feat: pokeApiService, for making requests

---
+++ src/app/services/poke-api.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { IPokedex, IPokeListItemModel, IPokeListModel } from '../interfaces';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PokeApiService {
+
+    /** URL base para busca na API */
+    private baseUrl: string = 'https://pokeapi.co/api/v2/pokemon/?limit=100&offset=0';
+
+    constructor(private http: HttpClient) { }
+
+    /**
+     * Recupera todos os pokemons e seus devidos stat's
+     */
+    get apiListAllPokemons(): Observable<IPokeListModel> {
+        return this.http.get<IPokeListModel>(this.baseUrl)
+            .pipe(
+                tap((response: IPokeListModel) => response),
+                tap((response: IPokeListModel) => {
+                    /** Passa por cada item da lista para pegar o getbyId do pokemon que está sendo iterado no momento */
+                    response.results.map((pokemon: IPokeListItemModel) => this.apiGetPokemons(pokemon.url)
+                        .subscribe({
+                            next: (pokemonSpecificData: IPokedex.Pokemon) => pokemonSpecificData,
+                            error: (error: any) => console.log(error),
+                        }));
+                })
+            );
+    }
+
+    /**
+     * Recupera os dados específicos de um pokemon.
+     *
+     * @param url Endpoint informado para recuperação dos dados do pokemon
+     */
+    public apiGetPokemons(url: string): Observable<IPokedex.Pokemon> {
+        return this.http.get<IPokedex.Pokemon>(url)
+            .pipe(map((pokemonSpecificData: IPokedex.Pokemon) => pokemonSpecificData))
+    }
+}
